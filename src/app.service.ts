4b67059b0a64d0d1948ba21f1f51b6faf27471a5
sanghoang1999 <hoangvansang846@gmail.com>
2020-11-18 00:06:53 +0700

    fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,10 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-<<<<<<< HEAD
-    return 'Hello World!';
-=======
     return 'New Work';
->>>>>>> 85c38ca55e1533c4fb7a750d51bf308be50b373d
   }
 }
